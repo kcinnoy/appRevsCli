8020a4fcf610bd6a712eebc419cd4f0dfe503c42
Yonnick <yonnick00@gmail.com>
2019-03-13 23:56:48 +0000

    added app class

---
+++ lib/appRevsCli/app.rb
@@ -0,0 +1,6 @@
+class AppRevsCli::App
+  attr_accessor :name, :rating, :review, :category
+
+  @@all = []
+  
+end
--- lib/appRevsCli/category.rb
+++ lib/appRevsCli/category.rb
@@ -1,3 +1,16 @@
-class appRevsCli::category
-  x
+class appRevsCli::Category
+
+  attr_accessor :name, :url, :application
+
+  @@all = []
+
+  def save
+    @@all << self
+  end
+
+  #class methods
+  def self.all
+    @@all
+  end
+
 end
